917db16ce64fd3cad15b3236e476dd49653acd54
Gina White <ginabythebay@gmail.com>
2025-06-08 10:57:47 -0700

    maybe 20 percent done.
    calc rpm, ipm

---
+++ src/mrr.js
@@ -0,0 +1,71 @@
+
+
+const possibleEvents = new Set(["input", "onpropertychange", "keyup", "change", "paste"]);
+
+window.onload = function () {
+  const diaInput = document.getElementById("dia")
+  const sfmInput = document.getElementById("sfm")
+  const flutesInput = document.getElementById("flutes")
+  const iptInput = document.getElementById("ipt")
+  const calculator = new Calculator(
+      diaInput, sfmInput, flutesInput, iptInput);
+  setLabel("rpm", "---")
+    possibleEvents.forEach(function (eventName) {
+    diaInput.addEventListener(eventName, function () {
+      calculator.calc();
+    });
+    sfmInput.addEventListener(eventName, function () {
+      calculator.calc();
+    });
+    flutesInput.addEventListener(eventName, function () {
+      calculator.calc();
+    });
+    iptInput.addEventListener(eventName, function () {
+      calculator.calc();
+    });
+  });
+
+}
+
+const Calculator =
+/** @class */
+function () {
+  function Calculator(diameterElement, sfmElement, flutesElement, iptElement) {
+    this.diameterElement = diameterElement;
+    this.sfmElement = sfmElement
+    this.flutesElement = flutesElement
+    this.iptElement = iptElement
+  }
+  Calculator.prototype.calc = function () {
+    let rpm = 0
+    const dia = Number(this.diameterElement.value)
+    const sfm = Number(this.sfmElement.value);
+    if (dia && sfm) {
+      rpm = Math.round(3.8197 / dia * sfm);
+      setLabel("rpm", rpm)
+    }
+
+    let ipm = 0
+    const flutes = Number(this.flutesElement.value)
+    const ipt = Number(this.iptElement.value)
+    if (rpm && flutes && ipt) {
+      ipm = rpm * ipt * flutes
+      setLabel("ipm", fixedDisplayNum(ipm, 1))
+    }
+  }
+  return Calculator;
+}();
+
+function fixedDisplayNum(value, precision) {
+  if (Number.isNaN(value) || value === Infinity || !value) {
+    return "--";
+  } else {
+    return value.toFixed(precision);
+  }
+}
+
+
+function setLabel(id, value) {
+  const output = document.getElementById(id);
+  output.innerHTML = String(value);
+}
