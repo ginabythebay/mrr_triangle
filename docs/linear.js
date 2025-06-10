83d7db5aee24f328fd307e7ec418f76eeb55527b
Gina White <ginabythebay@gmail.com>
2025-06-10 16:17:22 -0700

    license, copyright

--- docs/linear.js
+++ docs/linear.js
@@ -1,4 +1,12 @@
 
+/*
+  Copyright Gina White 2025
+
+  This Source Code Form is subject to the terms of the Mozilla Public
+  License, v. 2.0. If a copy of the MPL was not distributed with this
+  file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
 const fractionRe = /((\d+)\s+)?(\d+)\/(\d+)/;
 const mmRe = /(\d+(\.\d+)?)\s*mm/;
 
--- docs/mrr.js
+++ docs/mrr.js
@@ -1,5 +1,13 @@
 
 
+/*
+  Copyright Gina White 2025
+
+  This Source Code Form is subject to the terms of the Mozilla Public
+  License, v. 2.0. If a copy of the MPL was not distributed with this
+  file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
 const possibleEvents = new Set(["input", "onpropertychange", "keyup", "change", "paste"]);
 
 window.onload = function () {
